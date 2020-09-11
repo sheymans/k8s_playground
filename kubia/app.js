5d1eadef01bdc5bf71ef61945035bb6dfe67c83a
Stijn Heymans <stijn.heymans@gmail.com>
2020-09-11 07:43:59 -0700

    Fail server after 5 requests
    We're trying to test k8s liveness probe.

--- kubia/app.js
+++ kubia/app.js
@@ -3,10 +3,18 @@ const os = require('os');
 
 console.log("Kubia server starting...");
 
+var countRequests = 0;
+
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
+  if (countRequests > 4) {
+    console.log("Server returns 500 after 5 requests");
+    response.writeHead(500);
+    response.end("You've hit the max number of requests" + "\n");
+  }
   response.writeHead(200);
   response.end("You've hit " + os.hostname() + "\n");
+  countRequests++;
 };
 
 var www = http.createServer(handler);
